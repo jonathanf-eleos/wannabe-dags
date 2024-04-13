b2a37efdeb88e1457928d9af2eb6f0512e457d30
Jonathan Fishzon <jonathanf@Jonathans-MacBook-Pro.local>
2024-04-13 18:03:00 +0300

    added numpy, added memory&cpu intesive DAGs

---
+++ dags/mem_intensive_dag.py
@@ -0,0 +1,26 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+import numpy as np
+
+def memory_intensive_task():
+    # Simulate memory-intensive operation
+    large_array = np.random.rand(10**7)
+
+dag = DAG(
+    'memory_intensive_dag',
+    schedule_interval=None,
+    catchup=False,
+    default_args={
+        'owner': 'airflow',
+        'depends_on_past': False,
+        'start_date': datetime(2024, 4, 13),
+    },
+)
+
+task_memory_intensive = PythonOperator(
+    task_id='memory_intensive_task',
+    python_callable=memory_intensive_task,
+    dag=dag,
+)
+
+task_memory_intensive
